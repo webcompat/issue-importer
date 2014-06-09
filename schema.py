036b22a1db2b6952ed99137d86c38d36b80c6eee
Mike Taylor <miket@mozilla.com>
2014-06-09 13:14:17 -0500

    Add some commments for schema.py

--- schema.py
+++ schema.py
@@ -4,6 +4,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+'''Defines a schema to validate against:
+
+body: required, a string
+title: required, a string
+labels: optional, but if included, all strings
+'''
+
 SCHEMA = {
     "type": "object",
     "properties": {
